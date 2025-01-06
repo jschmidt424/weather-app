714c3b9fcab9c8163a7c26a5b01b03ffff38923e
spazdrum <spazdrum@gmail.com>
2025-01-06 14:05:12 -0500

    Theme Toggler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useContext } from "react";
+import ThemeContext from "./context/theme.context";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import "./styles/components/App.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
-  const dark = true;
+  const { dark } = useContext(ThemeContext);
   return (
     <div className={`App-${dark ? "dark" : ""}`}>
       <Header />
