be9e8ad1d64d758b82e0ad0d1a30e41160ee5c49
spazdrum <spazdrum@gmail.com>
2025-01-06 13:38:54 -0500

    Horizontal Scroll

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,15 +1,16 @@
 import DailyForecastWidget from "./DailyForecastWidget";
 import HourlyForecastWidget from "./HourlyForecastWidget";
 import "../styles/components/Forecast.scss";
+import HorizontallyScrollable from "./HorizontallyScrollable";
 
 function Forecast({ title, type, data }) {
   return (
     <div className="Forecast">
       <div className="forecast-container">
         <h3>{title}</h3>
-        <div className="widget-container">
+        <HorizontallyScrollable className="widget-container">
           {data.map((singleData) => (
-            <div>
+            <div key={singleData.date || singleData.day}>
               {type === "hourly" ? (
                 <HourlyForecastWidget data={singleData} />
               ) : (
@@ -17,7 +18,7 @@ function Forecast({ title, type, data }) {
               )}
             </div>
           ))}
-        </div>
+        </HorizontallyScrollable>
       </div>
     </div>
   );
